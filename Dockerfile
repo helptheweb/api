69c2851ab71af7cdfb0a479e53073e4151aad452
Ryan Long <rlong@redhat.com>
2024-10-21 16:20:41 -0400

    fix: :zap: add options

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,35 @@
-FROM oven/bun
-COPY . .
-RUN bunx @puppeteer/browsers install chrome@stable --path $HOME/.cache/puppeteer
+# Use an ARM64-compatible Bun image
+FROM oven/bun:1.0-alpine
+
+# Set the working directory in the container
+WORKDIR /usr/src/app
+
+# Install dependencies required for Puppeteer
+RUN apk add --no-cache \
+  chromium \
+  nss \
+  freetype \
+  harfbuzz \
+  ca-certificates \
+  ttf-freefont \
+  nodejs 
+
+# Copy package.json and bun.lockb (if available)
+COPY package.json ./
+COPY bun.lockb ./
+
+# Install dependencies
 RUN bun install
-CMD ["bun", "start"]
+
+# Copy the rest of your application's code
+COPY . .
+
+# Set environment variable to tell Puppeteer to use the installed Chromium
+ENV PUPPETEER_SKIP_CHROMIUM_DOWNLOAD true
+ENV PUPPETEER_EXECUTABLE_PATH /usr/bin/chromium-browser
+
+# Expose the port your app runs on
+EXPOSE 3000
+
+# Run your app
+CMD ["bun", "run", "start"]
---
+++ src/scanner/index.ts
@@ -0,0 +1,28 @@
+
+import { generateReport, generateMultipleReports } from './generateReport';
+import { displayResults } from './displayResults';
+import { getUrlsFromSitemap } from './getUrlsFromSitemap';
+import type { ReportInterface } from './types';
+import { type LaunchOptions } from 'puppeteer';
+
+export const scanner = async (url: string, puppeteerLaunchOptions?: LaunchOptions): Promise<ReportInterface[]> => {
+
+  let output: ReportInterface[] = [];
+
+  if (url && URL.canParse(url)) {
+    if (url.includes('.xml')) {
+      const fullSitemap: string[] = await getUrlsFromSitemap(url);
+      const reports = await generateMultipleReports(fullSitemap);
+      for (let report of reports) {
+        const formattedResults: ReportInterface = displayResults(report);
+        output.push(formattedResults);
+      }
+    } else {
+      const report = await generateReport(url, puppeteerLaunchOptions);
+      const formattedResults: ReportInterface = displayResults(report);
+      output.push(formattedResults);
+    }
+  }
+
+  return output;
+};
